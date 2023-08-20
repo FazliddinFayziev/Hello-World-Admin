df2fd22f062d57dfe7ac94519b25bc2e02e4561b
FazliddinFayziev <fazrez4515@gmail.com>
2023-08-20 22:06:02 +0800

    sixth commit

--- src/Pages/Notes.jsx
+++ src/Pages/Notes.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaTrash, FaEdit, FaSave } from 'react-icons/fa';
+import '../style/notes.css';
+import { SmallFooter } from '../components';
 
 const Notes = () => {
-    return (
-        <div>Notes</div>
-    )
-}
+    const [notes, setNotes] = useState([]);
+    const [noteInput, setNoteInput] = useState('');
+    const currentDate = new Date();
+    const [editingNoteIndex, setEditingNoteIndex] = useState(null);
 
-export default Notes
+    const handleAddNote = () => {
+        if (noteInput) {
+            setNotes([...notes, { text: noteInput, date: currentDate }]);
+            setNoteInput('');
+        }
+    };
+
+    const handleDeleteNote = (index) => {
+        const updatedNotes = notes.filter((_, i) => i !== index);
+        setNotes(updatedNotes);
+    };
+
+    const handleEditNote = (index) => {
+        setEditingNoteIndex(index);
+        setNoteInput(notes[index].text);
+    };
+
+    const handleSaveNote = () => {
+        const updatedNotes = [...notes];
+        updatedNotes[editingNoteIndex].text = noteInput;
+        setNotes(updatedNotes);
+        setEditingNoteIndex(null);
+        setNoteInput('');
+    };
+
+    return (
+        <>
+            <div className="notes-container">
+                <div className='notes__input__container'>
+                    <div className="notes-input">
+                        <input
+                            type="text"
+                            placeholder="Write a note . . ."
+                            value={noteInput}
+                            onChange={(e) => setNoteInput(e.target.value)}
+                        />
+                    </div>
+                    {editingNoteIndex !== null ? (
+                        <button className='save-button' onClick={handleSaveNote}>
+                            <FaSave />
+                        </button>
+                    ) : (
+                        <button className='add-button' onClick={handleAddNote}>
+                            Add
+                        </button>
+                    )}
+                </div>
+                <div className="notes-list">
+                    {notes.map((note, index) => (
+                        <div className='note-item-container' key={index}>
+                            <div className="note-item">
+                                <p>{note.text}</p>
+                            </div>
+                            <div className="note-buttons">
+                                {editingNoteIndex === index ? (
+                                    <button className='save-button' onClick={handleSaveNote}>
+                                        <FaSave />
+                                    </button>
+                                ) : (
+                                    <>
+                                        <button className='edit-button-notes' onClick={() => handleEditNote(index)}>
+                                            <FaEdit />
+                                        </button>
+                                        <button className='delete-button-notes' onClick={() => handleDeleteNote(index)}>
+                                            <FaTrash />
+                                        </button>
+                                    </>
+                                )}
+                            </div>
+                            <p className="note-date">{note.date.toLocaleString()}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <SmallFooter />
+        </>
+    );
+};
+
+export default Notes;
--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from 'react';
+import '../style/orders.css';
+
+const orders = [
+    {
+        cardItems: [
+            {
+                name: "Javascript T-Shirt",
+                image: "https://res.cloudinary.com/djijmzccq/image/upload/v1685875052/yellow-small_gom0xq.jpg",
+                price: 200000,
+                quantity: 2
+            },
+            {
+                name: "React T-Shirt",
+                image: "https://res.cloudinary.com/djijmzccq/image/upload/v1685871121/react-small_gvqick.jpg",
+                price: 200000,
+                quantity: 2
+            }
+        ],
+        totalPrice: 400,
+        userInfo: {
+            userName: "Fazliddin",
+            phoneNumber: "990194515",
+            avenue: "Toshkent",
+            address: "Sergeli-7 Ibrat Mahalla"
+        }
+    }
+]
 
 const Orders = () => {
     return (
-        <div>Orders</div>
-    )
-}
+        <div className="orders-container">
+            {orders.map((order, index) => (
+                <div className="order-item" key={index}>
+                    <p className='order__item__title'> • Products</p>
+                    <div className="order-summary">
+                        <div className="order-products">
+                            {order.cardItems.map((item, itemIndex) => (
+                                <div className="product-item" key={itemIndex}>
+                                    <img src={item.image} alt={item.name} />
+                                    <p>{item.name}</p>
+                                    <p>{item.price} UZS</p>
+                                    <p>Elegant</p>
+                                    <p>Quantity: {item.quantity}</p>
+                                </div>
+                            ))}
+                        </div>
 
-export default Orders
+                        <div className="total-price-container">
+                            <div className="total-price">
+                                <p>Total Price: <span>{order.totalPrice}  UZS</span></p>
+                            </div>
+                        </div>
+
+                        <p className='order__item__title'> • User Info</p>
+
+                        <div className="order-details">
+                            <p className='order__details__info'>Name:</p>
+                            <p>{order.userInfo.userName}</p>
+                            <p className='order__details__info'>Phone Number:</p>
+                            <p>{order.userInfo.phoneNumber}</p>
+                            <p className='order__details__info'>Address:</p>
+                            <p>{order.userInfo.avenue}</p>
+                            <p>{order.userInfo.address}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Orders;
